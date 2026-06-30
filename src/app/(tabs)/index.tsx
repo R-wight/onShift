d1886707c13f71315e29ccc48cd20877914b0c68
Rylan Wight <wight7143@saskpolytech.ca>
2026-06-29 18:13:12 -0600

    Story 1 - Button to start/end shift - Logic is working, basic style done

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
 // TODO: Need to add styling for the shift info components, shouldn't be in the center?
-import { Text, ScrollView, StyleSheet, Platform, View } from "react-native";
+import { Text, ScrollView, StyleSheet, Platform, View, TouchableOpacity } from "react-native";
 import {globalStyles} from "@/styles/global";
 import { colors } from "@/styles/global";
 import  HomeHeader  from "@/Components/homeHeader";
 import { Link } from "expo-router";
-import { getShifts, Shift } from "@/storage/shifts";
-import { useCallback, useState, useMemo } from 'react';
+import { getShifts, Shift, isOnShift, toggleShift, startShift, endShift } from "@/storage/shifts";
+import { useCallback, useState, useMemo, useEffect } from 'react';
 import { useFocusEffect } from 'expo-router';
 import  ShiftInfo  from '@/Components/shiftInfo'
 // useFocusEffect is used to update the shifts when switching between tabs
@@ -13,25 +13,15 @@ import  ShiftInfo  from '@/Components/shiftInfo'
 //Can use ScrollView instead of view if want to scroll
 
 export default function Index() {
-//   const [payDay, setPayDay] = useState<Date | null>(null); //new Date(2026, 5, 19);
-//   useEffect( () =>  {
-//     const initialPayDay = new Date(2026, 5, 19);
-//     const currDate = new Date();
-    
-//     while(currDate > initialPayDay){
-//       initialPayDay.setDate(initialPayDay.getDate() + 14);
-//     }
-//     setPayDay(initialPayDay);
-// });
 
+  // this function finds the next pay day to display on screen and to give a filter to the
+  // shown past shifts
 const payDay = useMemo(() => {
   const nextPayDay = new Date(2026, 5, 19);
   const currDate = new Date();
-
   while (currDate > nextPayDay) {
     nextPayDay.setDate(nextPayDay.getDate() + 14);
   }
-
   return nextPayDay;
 }, []);
 
@@ -43,25 +33,23 @@ const payDay = useMemo(() => {
     
     for(let i = 0; i < data.length; i++){
         // Need to create new dates because storage stores the dates as strings
-        let newShift: Shift = {
+      let newShift: Shift = {
         id: data[i].id,
         name: data[i].name,
         date: new Date(data[i].date),
         startTime: new Date(data[i].startTime),
         endTime: new Date(data[i].endTime)
+      }
+      const cutOffDate = new Date(payDay);
+      cutOffDate.setDate(cutOffDate.getDate() - 6)
+      cutOffDate.setHours(0,0,0,0);
+      const startPayPeriod = new Date(payDay);
+      startPayPeriod.setDate(startPayPeriod.getDate() - 21);
+      startPayPeriod.setHours(0,0,0,0);
+      if(newShift.date <= cutOffDate && newShift.date >= startPayPeriod){
+        parsedShifts.push(newShift);
+      }   
     }
-    const cutOffDate = new Date(payDay);
-    cutOffDate.setDate(cutOffDate.getDate() - 6)
-    cutOffDate.setHours(0,0,0,0);
-    const startPayPeriod = new Date(payDay);
-    startPayPeriod.setDate(startPayPeriod.getDate() - 21);
-    startPayPeriod.setHours(0,0,0,0);
-    if(newShift.date <= cutOffDate && newShift.date >= startPayPeriod){
-      parsedShifts.push(newShift);
-    }
-        
-    }
-    console.log(parsedShifts)
     parsedShifts.sort((a,b) => b.date.getTime() - a.date.getTime());
     setShifts(parsedShifts);
   } 
@@ -72,6 +60,31 @@ const payDay = useMemo(() => {
     }, []),
   );
 
+  const [onShift, setOnShift] = useState<boolean>();
+  const [btnText, setBtnText] = useState<string>();
+
+  const checkOnShift = async() => {
+    const data = await isOnShift();
+    console.log("Initial", data);
+    setOnShift(data);
+  }
+  useFocusEffect(
+    useCallback( () => {
+      checkOnShift();
+    }, []),
+  );
+
+  async function handleStartEndShift(){
+    if(onShift){
+      // end the shift and save the shift
+      await endShift();
+      setOnShift(false)
+      return;
+    }
+    // start the shift and save the start time
+    await startShift();
+    setOnShift(true);
+  }
 
   return (
     <View style={globalStyles.contWithHeader}>
@@ -84,7 +97,10 @@ const payDay = useMemo(() => {
       }) : "Error"}</Text>
       <HomeHeader />
       <ScrollView style={{width: "100%"}} contentContainerStyle={{alignItems: "center"}} > 
-      <ShiftInfo shifts={shifts} title={"Current Pay Period"} custStyles={styles.shifts} cardStyle={styles.cardStyle}/>
+        <TouchableOpacity style={styles.button} onPress={handleStartEndShift}>
+          <Text style={styles.buttonText}>{onShift ? "End Shift" : "Start Shift"}</Text>
+        </TouchableOpacity>
+        <ShiftInfo shifts={shifts} title={"Current Pay Period"} custStyles={styles.shifts} cardStyle={styles.cardStyle}/>
       </ScrollView>
     </View>
   );
@@ -100,5 +116,19 @@ const styles = StyleSheet.create({
   },
   cardStyle: {
     width: "45%",
-  }
+  },
+  button: {
+    backgroundColor: '#4fc3f7',
+    padding: 16,
+    borderRadius: 75,
+    alignItems: 'center',
+    marginTop: 24,
+    marginBottom: 20,
+    width: '50%'
+  },
+  buttonText: {
+    color: '#1a1a2e',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
--- src/storage/shifts.ts
+++ src/storage/shifts.ts
@@ -9,6 +9,9 @@ export type Shift = {
 };
 
 const SHIFTS_KEY = 'shifts';
+const ONSHIFT_KEY = 'onshift';
+const STARTTIME_KEY = 'starttime';
+const ENDTIME_KEY = 'endtime';
 
 export const getShifts = async (): Promise<Shift[]> => {
     const data = await AsyncStorage.getItem(SHIFTS_KEY);
@@ -27,3 +30,45 @@ export const addShift = async(
     await AsyncStorage.setItem(SHIFTS_KEY, JSON.stringify([newShift, ...shifts]));
     return newShift;
 };
+
+export const toggleShift = async(): Promise<boolean> => {
+    if(await isOnShift()){
+        await AsyncStorage.setItem(ONSHIFT_KEY, JSON.stringify(false));
+        return false;
+    }
+    await AsyncStorage.setItem(ONSHIFT_KEY, JSON.stringify(true));
+    return true;
+}
+
+export const isOnShift = async(): Promise<boolean> => {
+    const data = await AsyncStorage.getItem(ONSHIFT_KEY);
+    if(data === "true"){
+        return true;
+    }
+    return false;
+}
+
+export const startShift = async(): Promise<void> => {
+    await AsyncStorage.setItem(STARTTIME_KEY, JSON.stringify((new Date()).getTime()));
+    await AsyncStorage.setItem(ONSHIFT_KEY, JSON.stringify(true));
+    return;
+}
+
+export const endShift = async(): Promise<void> => {
+    const currentDate = new Date();
+    const start = await AsyncStorage.getItem(STARTTIME_KEY);
+    await AsyncStorage.setItem(ONSHIFT_KEY, JSON.stringify(false));
+    if(!start){
+        return;
+    }
+    const startTime = new Date(parseFloat(start));
+    await addShift({
+            name: "Job",
+            date: currentDate,
+            startTime: startTime,
+            endTime: currentDate
+        });
+    await AsyncStorage.removeItem(STARTTIME_KEY);
+    
+    return;
+}
